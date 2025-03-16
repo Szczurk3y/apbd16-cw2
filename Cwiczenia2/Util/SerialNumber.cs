70dd37580596e69b4cbf6d7906f27340fed5904d
s24196 <s24196@pjwstk.edu.pl>
2025-03-16 16:56:34 +0100

    Add base class, container, serial number

---
+++ Cwiczenia2/Util/SerialNumber.cs
@@ -0,0 +1,22 @@
+﻿namespace Cwiczenia2.Util;
+
+public class SerialNumber
+{
+    private static readonly List<SerialNumber> ContainerList = [];
+    private const string Format = "KON";
+
+    private readonly ContainerType _containerType;
+    private readonly int _containerNumber;
+    
+    public SerialNumber(ContainerType containerType)
+    {
+        ContainerList.Add(this);
+        _containerType = containerType;
+        _containerNumber = ContainerList.Count;
+    }
+
+    public string GetSerialNumber()
+    {
+        return Format + '-' + _containerType + '-' + _containerNumber;
+    }
+}
